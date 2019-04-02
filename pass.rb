99e0042d6c85a0360f1204236b478808d4e4d877
Max <badmax77@gmail.com>
2019-04-03 00:31:10 +0400

    1.11 method & class

--- pass.rb
+++ pass.rb
@@ -1,57 +1,43 @@
 
 
-=begin
-По-хорошему, описание классов и методов складируются в отдельные файлы,
-а исполняемая логика находится в "Главном файле"
-=end
-
-
 # standart pass
-def hardpass(x)
+def hrdpas(x)
   mas = ['a'..'z', 'A'..'Z', '0'..'9'].map(&:to_a).flatten
   (0..x).map { mas.sample }.join
 end
 i = 6
-puts "Easy pass = #{hardpass(i)}" #метод
+puts "Easy pass = #{hrdpas(i)}"
 
 # easyread pass
 class Hpass
   def initialize(length)
-    glasnie = %w[A E I O U]
-    soglasnie = %w[B C D F G H J K L M N P Q R S T V W X Y Z]
+    v = %w[A E I O U]
+    c = %w[B C D F G H J K L M N P Q R S T V W X Y Z]
     symb = %w[! @ # $ % ^ & *]
     num = rand(9)
 
-    pas2 = []
+    pass = []
     i = 1
     while i < length + 1
-      case i
-      when 1
-        pas2 << num
-      when length
-        pas2 << symb.sample
-      else if i % 2 == 0
-        pas2 << if rand(3) == 1
-                  soglasnie.sample
-                else
-                  soglasnie.sample.downcase
-                end
-           else
-        pas2 << if rand(3) == 1
-                  glasnie.sample
-                else
-                  glasnie.sample.downcase
-                end
-           end
-      end
+      pass << case i
+              when 1
+                num
+              when length
+                symb.sample
+              else if i.even?
+	                   rand(2) == 1 ? v.sample : v.sample.downcase
+                   else
+                     rand(2) == 1 ? c.sample : c.sample.downcase
+                   end
+              end
       i += 1
     end
-    print "your pass: #{pas2.join}"
+    print "your pass: #{pass.join}"
   end
 end
 
 p 'Enter the number of characters for the password: '
 x = gets.to_i
 
-Hpass.new(x) #объект класса
+Hpass.new(x)
 
